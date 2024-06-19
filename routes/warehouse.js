aac569fadf20cd81588104c696fcb349506f7f8f
Bao <bao9600455@yahoo.com.vn>
2024-06-19 14:38:11 -0400

    add edit method to inventory list

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,29 +1,24 @@
-import express from 'express'
-import knex from 'knex';
+import express from "express";
+import knex from "knex";
 const router = express.Router();
 
-router
-    .route('/')
-    .get( async (_req, res) => {
-        try {
-            const listOfWarehouses = await knex.select('*').from('warehouses')
-            res.json(listOfWarehouses)
-        } catch {
-            return res.status(500).send('Error getting Warehouses')
-        }
-})
+router.route("/").get(async (_req, res) => {
+  try {
+    const listOfWarehouses = await knex.select("*").from("warehouses");
+    res.json(listOfWarehouses);
+  } catch {
+    return res.status(500).send("Error getting Warehouses");
+  }
+});
 
-router
-    .route('/:id')
-    .get( async (req, res) => {
-        try {
-            const id = req.params.id
-            const warehouse = await knex.select(id).from('warehouses')
-            res.json(warehouse)
-        } catch {
-            return res.status(500).send('Error getting Warehouse')
+router.route("/:id").get(async (req, res) => {
+  try {
+    const id = req.params.id;
+    const warehouse = await knex.select(id).from("warehouses");
+    res.json(warehouse);
+  } catch {
+    return res.status(500).send("Error getting Warehouse");
+  }
+});
 
-        }
-    })
-
-export default router
+export default router;
