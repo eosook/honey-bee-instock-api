b60574389818bee30cfce3c9b00d26609f80a507
Ogi Dimitrijevic <ogi@ogidesigns.ca>
2024-06-21 10:56:55 -0400

    gommiting

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
-app.use("/", warehouse);
 app.use("/warehouse", warehouse);
 app.use("/inventory", inventories);
 // start Express on port 8080
