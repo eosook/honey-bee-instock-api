4c407e5f94fd62205b369c61b351bb4fc75c494e
Jenna Knechtel <jmknec@gmail.com>
2024-06-21 10:09:04 -0400

    start api put request for warehouse

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -90,7 +90,7 @@ router
           message: "Warehouse added successfully ",
           warehouse: newWarehouse,
         });
-      } catch (error) {
+      } catch {
         return res.status(400).send("Error adding warehouse");
       }
     }
@@ -121,7 +121,92 @@ router
     } catch {
       return res.status(404).send("Warehouse ID not found");
     }
-  });
+  })
+  .put(
+    checkSchema(
+      {
+        warehouse_name: {
+          trim: true,
+          notEmpty: { bail: true },
+          errorMessage: "This is a required field",
+        },
+        address: {
+          trim: true,
+          notEmpty: { bail: true },
+          errorMessage: "This is a required field",
+        },
+        city: {
+          trim: true,
+          notEmpty: { bail: true },
+          errorMessage: "This is a required field",
+        },
+        country: {
+          trim: true,
+          notEmpty: { bail: true },
+          errorMessage: "This is a required field",
+        },
+        contact_name: {
+          trim: true,
+          notEmpty: { bail: true },
+          errorMessage: "This is a required field",
+        },
+        contact_position: {
+          trim: true,
+          notEmpty: { bail: true },
+          errorMessage: "This is a required field",
+        },
+        contact_phone: {
+          trim: true,
+          matches: {
+            options: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+          },
+        },
+        contact_email: { trim: true, isEmail: { bail: true } },
+      },
+      ["body"]
+    ),
+    async (req, res) => {
+      const id = req.params.id;
+      const {
+        warehouse_name,
+        address,
+        city,
+        country,
+        contact_name,
+        contact_position,
+        contact_phone,
+        contact_email,
+      } = req.body;
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        return res.status(400).json({ errors: result.array() });
+      }
+      try {
+        const warehouse = await knex
+          .select("*")
+          .from("warehouses")
+          .where("id", id);
+        if (!id) {
+          return res.status(404).send("Warehouse not found");
+        }
+        const editedWarehouse = {
+          id,
+          warehouse_name,
+          address,
+          city,
+          country,
+          contact_name,
+          contact_position,
+          contact_phone,
+          contact_email,
+        };
+        console.log(warehouse, editedWarehouse);
+        return res.send("found the warehouse!");
+      } catch {
+        res.status(500).send("Error updating warehouse");
+      }
+    }
+  );
 
 router.route("/:id/inventories").get(async (req, res) => {
   const id = req.params.id;
